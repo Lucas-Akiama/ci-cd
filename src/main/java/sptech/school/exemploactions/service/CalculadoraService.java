a546442f601dfc0fedb3155cb0f77c8d6d20befd
Lucas-Akiama <lucas.brasiliano@sptech.school>
2024-06-02 16:51:38 -0300

    first commit

---
+++ src/main/java/sptech/school/exemploactions/service/CalculadoraService.java
@@ -0,0 +1,15 @@
+package sptech.school.exemploactions.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+public class CalculadoraService {
+    public double dividir(double a, double b){
+        if(b == 0){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Divisao por zero");
+        }
+        return a / b;
+    }
+}
