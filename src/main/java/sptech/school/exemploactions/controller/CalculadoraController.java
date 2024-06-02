77da52dbdc920963e5c2e8a5e702266cd9c06a1f
Lucas-Akiama <lucas.brasiliano@sptech.school>
2024-06-02 17:13:44 -0300

    terceiro commit, na wsl

--- src/main/java/sptech/school/exemploactions/controller/CalculadoraController.java
+++ src/main/java/sptech/school/exemploactions/controller/CalculadoraController.java
@@ -7,11 +7,9 @@ import sptech.school.exemploactions.service.CalculadoraService;
 @RestController
 public class CalculadoraController {
     private final CalculadoraService calculadoraService;
-
     public CalculadoraController(CalculadoraService calculadoraService){
         this.calculadoraService = calculadoraService;
     }
-
     @PostMapping("/divisao/{a}/{b}")
     public ResponseEntity<Double> divisao(@PathVariable double a, @PathVariable double b){
         return ResponseEntity.ok(calculadoraService.dividir(a, b));
